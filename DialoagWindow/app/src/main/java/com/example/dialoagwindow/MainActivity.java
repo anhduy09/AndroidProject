000a42ce74cc8ca6a4ef8db4b432065c7c42b2e8
anhduy09 <anhduys2v@gmail.com>
2023-02-23 11:10:10 -0500

    Dialog window application

---
+++ DialoagWindow/app/src/main/java/com/example/dialoagwindow/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.dialoagwindow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MyDialog myDialog = new MyDialog();
+                myDialog.show(getSupportFragmentManager(),"123");
+            }
+        });
+    }
+}
---
+++ DialoagWindow/app/src/main/java/com/example/dialoagwindow/MyDialog.java
@@ -0,0 +1,35 @@
+package com.example.dialoagwindow;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+//import androix.appcompat.app.AlertDialog;
+
+public class MyDialog extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Make a selection")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //
+                    }
+                })
+                .setNegativeButton("Cancle", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
