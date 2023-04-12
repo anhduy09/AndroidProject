fbf16ff7a214b4f4f8412b8b8748b01136f411f0
duy <anhduys2v@gmail.com>
2023-04-12 11:06:31 -0400

    Simple fragment app

---
+++ SimpleFragment/app/src/main/java/com/example/simplefragment/SearchFragment.java
@@ -0,0 +1,24 @@
+package com.example.simplefragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class SearchFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.content_search, container, false);
+
+        //database and UI
+
+        return view;
+    }
+}
